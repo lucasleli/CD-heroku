ac636237d92b7b5259986d1d4c207899a3377f1c
lucasleli <lucas.leli10@gmail.com>
2020-04-15 11:20:27 -0300

    Atualização Index

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Carai cachoeira, DEU CERTO"
+    return "Carai cachoeira, DEU CERTO MESMO"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
